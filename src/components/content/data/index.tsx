390b225113e0be1dcf0ccdf5968ab856eb13e1e9
Roya90 <r_azemi@yahoo.com>
2026-01-05 21:00:18 -0500

    feat(string-file): data file

--- src/components/content/data/index.tsx
+++ src/components/content/data/index.tsx
@@ -1,4 +1,7 @@
 import { PaintModal, RecycleBin, MusicPlayer, AboutMe, ContactMe, Resume, MyComputer, MyProjects } from '..';
+import text from '@/content/text.json';
+
+const dataText = text.data;
 
 export const MODAL_DATA: Record<
   string,
@@ -14,32 +17,32 @@ export const MODAL_DATA: Record<
   }
 > = {
   projects: {
-    title: 'C:\\My Projects',
+    title: dataText.myProjects,
     icon: '📂',
     headerColor: 'from-pink-400 to-violet-400',
     content: <MyProjects />,
   },
   about: {
-    title: 'C:\\About Me',
+    title: dataText.aboutMe,
     icon: '📝',
     headerColor: 'from-purple-600 to-cyan-300',
     content: <AboutMe />,
   },
   contact: {
-    title: 'C:\\Contact Me',
+    title: dataText.contactMe,
     icon: '📧',
     headerColor: 'from-amber-400 to-fuchsia-400',
     content: <ContactMe />,
   },
   resume: {
-    title: 'C:\\Resume.txt',
+    title: dataText.resume,
     icon: '📝',
     headerColor: 'from-blue-400 to-gray-500',
     content: <Resume />,
     // scrollable: false,
   },
   paint: {
-    title: 'Untitled - Paint',
+    title: dataText.untitledPaint,
     icon: '🎨',
     headerColor: 'from-teal-500 to-indigo-400',
     content: <PaintModal />,
@@ -48,20 +51,20 @@ export const MODAL_DATA: Record<
     height: 'aspect-[4/3]',
   },
   recycle: {
-    title: 'Recycle Bin',
+    title: dataText.bin,
     icon: '🗑️',
     headerColor: 'from-amber-300 to-amber-100',
     content: <RecycleBin />,
   },
   vibes: {
-    title: 'Vibes Player',
+    title: dataText.player,
     icon: '🎶',
     headerColor: 'from-teal-400 to-amber-200',
     content: <MusicPlayer />,
     noPadding: true,
   },
   computer: {
-    title: 'My Computer',
+    title: dataText.myComputer,
     icon: '💻',
     headerColor: 'from-pink-300 to-violet-500',
     content: <MyComputer onClose={() => {}} />,
